d6f80e90aa09a7f64ce1ce2936557d34a279c2f8
Will <sylus1984@gmail.com>
2013-09-29 18:50:42 -0400

    Improve the frontend developer experience to more resemble how wetkit_omega does things.

--- config.rb
+++ config.rb
@@ -1,24 +1,54 @@
-# Require any additional compass plugins here.
-require 'modular-scale'
+##
+## This file is only needed for Compass/Sass integration. If you are not using
+## Compass, you may safely ignore or delete this file.
+##
+## If you'd like to learn more about Sass and Compass, see the sass/README.txt
+## file for more information.
+##
 
+# Default to development if environment is not set.
+environment = :production
+saved = environment
+if (environment.nil?)
+  environment = :development
+else
+  environment = saved
+end
+
+# Location of the theme's resources.
 http_path = "/"
 css_dir = "css"
 sass_dir = "sass"
 images_dir = "images"
 fonts_dir = "fonts"
-javascripts_dir = "scripts"
+generated_images_dir = images_dir + "/generated"
+javascripts_dir = "js"
 
-add_import_path "lib/ux_styleguide"
+# Require any additional compass plugins installed on your system.
+require 'compass-normalize'
+require 'modular-scale'
+require 'rgbapng'
+require 'toolkit'
+require 'sass-globbing'
 
-# output_style = :expanded or :nested or :compact or :compressed
-output_style = :expanded
+##
+## You probably don't need to edit anything below this.
+##
 
+# You can select your preferred output style here (:expanded, :nested, :compact
+# or :compressed).
+output_style = (environment == :production) ? :expanded : :nested
+
+# To enable relative paths to assets via compass helper functions. Since Drupal
+# themes can be installed in multiple locations, we don't need to worry about
+# the absolute path to the theme from the server omega.
 relative_assets = true
 
-# line_comments and debug_info can be activated by setting the environment
-# flag to "development"
-line_comments = (environment == :development) ? true : false
+# Conditionally enable line comments when in development mode.
+line_comments = (environment == :production) ? false : true
 
-sass_options = {
-  :debug_info => (environment == :development) ? true : false
-}
+# Output debugging info in development mode.
+sass_options = (environment == :production) ? {} : {:debug_info => true}
+
+# Add the 'sass' directory itself as an import path to ease imports.
+add_import_path 'sass'
--- theme-settings.php
+++ theme-settings.php
@@ -9,15 +9,5 @@
  * Implements THEME_form_system_theme_settings_alter().
  */
 function wetkit_ember_form_system_theme_settings_alter(&$form, &$form_state) {
-  $form['wetkit_ember'] = array(
-    '#type' => 'fieldset',
-    '#title' => t('WxT Ember settings'),
-    '#description' => t('If checked, the fade-in effect between page loads will not occur.'),
-  );
-  $form['wetkit_ember']['wetkit_ember_no_fadein_effect'] = array(
-    '#type' => 'checkbox',
-    '#title' => t('Disable fade-in effect'),
-    '#default_value' => theme_get_setting('wetkit_ember_no_fadein_effect'),
-    '#description' => t('Check here if you want the theme to fade in pages when they load.'),
-  );
+
 }
